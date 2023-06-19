caed6c9ea1a157b63b0db60b3a7cb4615504eff3
amitaimacklin <amitaimacklin@gmail.com>
2023-06-18 23:25:14 -0400

    Added player class, need to add heroes at entrance

--- player.py
+++ player.py
@@ -1,2 +1,23 @@
+from dungeon import Dungeon
+
 class Player():
-    pass
+
+    def __init__(self, hand, boss):
+        self._dungeon = Dungeon(boss)
+        self._hand = hand
+    
+    def get_card(self, cardName):
+        for i in self._hand:
+            if i.cardName == cardName:
+                return i
+            
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def get_hand(self):
+        return self._hand
+    
+    def remove_card(self, cardName):
+        for i in 1..len(self._hand):
+            if self._hand.i.cardName == cardName:
+                del self._hand.i
