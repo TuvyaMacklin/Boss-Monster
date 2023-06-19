5b543aad972c2c77f005ba37bd1c143f66dcbf2c
amitaimacklin <amitaimacklin@gmail.com>
2023-06-18 23:32:28 -0400

    Added heroes at entrance

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player():
     def __init__(self, hand, boss):
         self._dungeon = Dungeon(boss)
         self._hand = hand
+        self._heroes_at_entrance = []
     
     def get_card(self, cardName):
         for i in self._hand:
@@ -18,6 +19,14 @@ class Player():
         return self._hand
     
     def remove_card(self, cardName):
-        for i in 1..len(self._hand):
+        for i in 0..len(self._hand):
             if self._hand.i.cardName == cardName:
                 del self._hand.i
+    
+    def add_hero(self, hero):
+        self._heroes_at_entrance.append(hero)
+
+    def remove_hero(self, heroName):
+        for i in 0..len(self._heroes_at_entrance):
+            if self._heroes_at_entrance.i.heroName == heroName:
+                del self._heroes_at_entrance.i
