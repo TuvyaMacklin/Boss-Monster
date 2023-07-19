8a8e2e23fcb296f18c75d1462320ec36192cb0eb
amitaimacklin <amitaimacklin@gmail.com>
2023-07-18 20:02:56 -0400

    Added the test file for misc tests

---
+++ test/test.py
@@ -0,0 +1,60 @@
+import csv
+from card import *
+from game import Game
+from deck import Deck
+
+def cardify(details):
+   match details[5]:
+      case "Boss":
+         return BossCard(details[1], details[6], details[4])
+      case "Room":
+         match details[2]:
+            case "Monster Room":
+                return RoomCard(details[1], details[6], details[3], [details[4]], "Monster", False)
+            case "Trap Room": 
+                return RoomCard(details[1], details[6], details[3], [details[4]], "Trap", False)
+            case "Advanced Monster Room":
+                return RoomCard(details[1], details[6], details[3], [details[4]], "Monster", True)
+            case "Advanced Trap Room":
+                return RoomCard(details[1], details[6], details[3], [details[4]], "Trap", True)
+      case "Hero":
+         match details[2]:
+            case "Ordinary Hero":
+                return HeroCard(details[1], details[6], details[3], details[4], 1)
+            case "Epic Hero":
+                return HeroCard(details[1], details[6], details[3], details[4], 2)
+
+decks = {
+   "hero_deck":[],
+   "epic_hero_deck":[],
+   "room_deck":[],
+   "boss_deck":[],
+   "spell_deck":[],
+}
+
+def loadCards():
+    with open("./boss_monster_cards.csv", 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            match row[5]:
+                case "Boss":
+                    for i in range(int(row[8])):
+                        decks["boss_deck"].append(cardify(row))
+                case "Hero":
+                    for i in range(int(row[8])):
+                        decks["hero_deck"].append(cardify(row))
+                case "Room":
+                    for i in range(int(row[8])):
+                        decks["room_deck"].append(cardify(row))
+                case "Spell":
+                    for i in range(int(row[8])):
+                        decks["spell_deck"].append(row)
+        decks["boss_deck"] = Deck(decks["boss_deck"])
+        decks["epic_hero_deck"] = Deck(decks["epic_hero_deck"])
+        decks["hero_deck"] = Deck(decks["hero_deck"])
+        decks["room_deck"] = Deck(decks["room_deck"])
+        decks["spell_deck"] = Deck(decks["spell_deck"])
+        print(decks)
+loadCards()
+gameManager = Game(decks)
+print(gameManager._getInput("Pick a card: ", 1, 5))
